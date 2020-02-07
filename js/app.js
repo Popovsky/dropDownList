809dc391eb79c3507189cc74bbd2de158ea65e91
pad19 <pad19051999@gmail.com>
2020-02-07 16:04:33 +0200

    second commit

--- js/app.js
+++ js/app.js
@@ -6,12 +6,23 @@ const ul = document.querySelectorAll('ul.list-unstyled.main ul');
 [].map.call(a, (el) => el.addEventListener('click', (e) => {
     e.preventDefault();
     [...a].map(el => el.classList.remove('selected'));
-    !e.target.parentNode.parentNode.classList.contains('open') ? [...ul].map(el => el.classList.remove('open')) : console.log('false');
-// childNodes
-    console.log(e.target.parentNode.parentNode.childNodes);
-    console.log(e.target.previousElementSibling);
-    if(e.target.previousElementSibling){
-        e.target.previousElementSibling.classList.add('open');
-    }
+    !e.target.parentNode.parentNode.classList.contains('open')
+        ?
+        [...ul].map(el => el.classList.remove('open'))
+        :
+        e.target.parentNode.parentNode.classList.contains('open')
+            ?
+            [...ul].map(el => el === e.target.parentNode.parentNode
+                ?
+                [...el.children].map( elem => [...elem.children].map(element => element.previousElementSibling
+                    ?
+                    element.previousElementSibling.classList.remove('open')
+                    :
+                    null))
+                :
+                null)
+            :
+            null;
+    e.target.previousElementSibling ? e.target.previousElementSibling.classList.add('open') : null;
     e.target.classList.add('selected');
 }));

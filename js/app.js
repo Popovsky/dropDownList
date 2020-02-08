bec75cf99ebfdc13e446bf602d2719f5a9099a70
pad19 <pad19051999@gmail.com>
2020-02-08 12:45:47 +0200

    third commit

--- js/app.js
+++ js/app.js
@@ -10,19 +10,17 @@ const ul = document.querySelectorAll('ul.list-unstyled.main ul');
         ?
         [...ul].map(el => el.classList.remove('open'))
         :
-        e.target.parentNode.parentNode.classList.contains('open')
+        [...ul].map(el => el === e.target.parentNode.parentNode
             ?
-            [...ul].map(el => el === e.target.parentNode.parentNode
-                ?
-                [...el.children].map( elem => [...elem.children].map(element => element.previousElementSibling
-                    ?
-                    element.previousElementSibling.classList.remove('open')
-                    :
-                    null))
-                :
-                null)
+            [...el.children].map(elem => [...elem.children].map(element => element.previousElementSibling
+                ? (
+                    element.previousElementSibling.classList.remove('open'),
+                    [...element.previousElementSibling.children].map(el => [...el.children].map(elem => elem.previousElementSibling ? elem.previousElementSibling.classList.remove('open') : null))
+                ) :
+                null))
             :
-            null;
+            null);
+
     e.target.previousElementSibling ? e.target.previousElementSibling.classList.add('open') : null;
     e.target.classList.add('selected');
 }));
